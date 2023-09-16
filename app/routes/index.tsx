94512ed38eb244714b008520c3cdeee3153ad43e
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2023-09-16 13:01:41 +0100

    Add padding to bottom navlinks on mobile, to make space for bottom URL bar. Set fly region to cdg.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -41,7 +41,7 @@ export default function Index() {
                 variants={containerVariants}
                 initial="hidden"
                 animate="visible"
-                className="absolute bottom-0 left-1/2 right-0 z-10 flex w-full -translate-x-1/2 flex-wrap justify-center gap-x-6 gap-y-1 py-5 md:bottom-auto md:left-auto md:top-1/2 md:w-fit md:-translate-y-1/2 md:flex-col md:items-end md:gap-y-6 md:p-0"
+                className="absolute bottom-10 left-1/2 right-0 z-10 flex w-full -translate-x-1/2 flex-wrap justify-center gap-x-6 gap-y-1 py-5 md:bottom-auto md:left-auto md:top-1/2 md:w-fit md:-translate-y-1/2 md:flex-col md:items-end md:gap-y-6 md:p-0"
             >
                 <MotionLink variants={linkVariants} to="/projects">
                     Projects
