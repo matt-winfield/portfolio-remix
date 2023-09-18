64dbe18fd3df8f23b0dfc1fdb7544399c59990f6
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2023-09-18 12:02:14 +0100

    Adjust skills styling

--- app/routes/skills.tsx
+++ app/routes/skills.tsx
@@ -35,22 +35,24 @@ export default function Skills() {
                 Skills
                 <motion.div className="absolute -bottom-1 h-[2px] w-full bg-accent-foreground" />
             </motion.div>
-            <motion.div
-                initial="hidden"
-                animate="visible"
-                transition={{ staggerChildren: 0.1 }}
-                className="flex flex-wrap items-center justify-center gap-2"
-            >
-                {data.technologies.map((technology) => (
-                    <motion.div
-                        variants={skillsVariants}
-                        key={technology.id}
-                        className="rounded-md bg-card px-3 py-2 text-xl"
-                    >
-                        {technology.name}
-                    </motion.div>
-                ))}
-            </motion.div>
+            <div className="flex flex-col items-center justify-center">
+                <motion.div
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ staggerChildren: 0.1 }}
+                    className="flex max-w-3xl flex-wrap items-center justify-center gap-5"
+                >
+                    {data.technologies.map((technology) => (
+                        <motion.div
+                            variants={skillsVariants}
+                            key={technology.id}
+                            className="rounded-md bg-card px-3 py-2 text-xl"
+                        >
+                            {technology.name}
+                        </motion.div>
+                    ))}
+                </motion.div>
+            </div>
         </div>
     );
 }
